a4eaa01ea6e230e193207418c109e84b4190bf96
Zubiarrain <zubiarrainnahuel@gmail.com>
2024-09-24 13:18:14 -0300

    test: pruebo incluir autenticacion dentro de la funcion

--- src/app/api/menu/route.ts
+++ src/app/api/menu/route.ts
@@ -5,18 +5,19 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 
-const auth = new google.auth.GoogleAuth({
-    credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL || "",
-        private_key: process.env.GOOGLE_PRIVATE_KEY || "", 
-    },
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'] 
-});
 
-const spreadsheetId = process.env.SPREADSHEET_ID
 
 const getGoogleSheetData = async () => {
   try {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+          client_email: process.env.GOOGLE_CLIENT_EMAIL || "",
+          private_key: process.env.GOOGLE_PRIVATE_KEY || "", 
+      },
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'] 
+    });
+  
+    const spreadsheetId = process.env.SPREADSHEET_ID
     console.log('busco sheets...')
     const sheets = google.sheets({ version: 'v4', auth });
     console.log('obtengo datos...')
